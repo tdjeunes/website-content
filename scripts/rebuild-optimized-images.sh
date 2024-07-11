6f7873f9ada0f3db283315cc9c3a19427bdc54ba
Albert Albala <albert@mediatribe.net>
2024-07-11 13:29:58 -0400

    Please squash this commit in the pull request

--- scripts/rebuild-optimized-images.sh
+++ scripts/rebuild-optimized-images.sh
@@ -7,4 +7,6 @@ set -e
 git clone https://github.com/dcycle/thumbor-example.git
 mv docs/media thumbor-example/my-media
 cd thumbor-example
+echo "xxxx"
+ls -lah
 ./scripts/generate-image-map.sh ./my-media http://contenu.terredesjeunes.org 800x mapping.json

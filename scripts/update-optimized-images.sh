b4d67b34b07d6a8402e0ed645abf72d193e950ee
Albert Albala <albert@mediatribe.net>
2024-07-11 12:41:44 -0400

    Please squash this commit in the pull request

--- scripts/update-optimized-images.sh
+++ scripts/update-optimized-images.sh
@@ -12,5 +12,4 @@ cd ./unversioned/optimized-images
 git clone https://github.com/dcycle/thumbor-example.git
 cp -r ../../docs/media ./thumbor-example/my-media
 cd thumbor-example
-pip3 install python-dotenv libthumbor
 python3 ./scripts/generate-image-map.py

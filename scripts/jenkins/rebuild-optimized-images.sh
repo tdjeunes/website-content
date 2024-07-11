648af3bb38aa2991cfff1effcc557caab92aa842
Albert Albala <albert@mediatribe.net>
2024-07-11 15:38:12 -0400

    Please squash this commit in the pull request

--- scripts/jenkins/rebuild-optimized-images.sh
+++ scripts/jenkins/rebuild-optimized-images.sh
@@ -22,27 +22,26 @@ if [ -v "$TDJ_IMAGE_MAP_DEPLOY_PRIVATE_KEY" ]; then
   exit 1
 fi
 
-# # Create a droplet
-# DROPLET_NAME=docker-tdj-images
+# Create a droplet
+DROPLET_NAME=docker-tdj-images
 
-# doctl compute droplet create "$DROPLET_NAME" --size 4gb --image ubuntu-20-04-x64 --ssh-keys "$DIGITALOCEAN_SSH_KEY"
+doctl compute droplet create "$DROPLET_NAME" --size 4gb --image ubuntu-20-04-x64 --ssh-keys "$DIGITALOCEAN_SSH_KEY"
 
-# sleep 45
+sleep 45
 
-# IP=$(doctl compute droplet get "$DROPLET_NAME" --format PublicIPv4 --no-header)
-# echo "Created Droplet at $IP"
+IP=$(doctl compute droplet get "$DROPLET_NAME" --format PublicIPv4 --no-header)
+echo "Created Droplet at $IP"
 
-# sleep 45
+sleep 45
 
-# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-#   root@"$IP" "mkdir -p docker-tdj-images"
-# scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-#   -r * root@"$IP":docker-tdj-images
-# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-#   root@"$IP" "cd docker-tdj-images && export THUMBOR_SECURITY_KEY=$THUMBOR_SECURITY_KEY && ./scripts/install-docker-and-rebuild-optimized-images.sh"
-# scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-#   root@"$IP":docker-tdj-images/thumbor-example/mapping.json mapping.json
-echo "HELLO WORLD" > mapping.json
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "mkdir -p docker-tdj-images"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  -r * root@"$IP":docker-tdj-images
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "cd docker-tdj-images && export THUMBOR_SECURITY_KEY=$THUMBOR_SECURITY_KEY && ./scripts/install-docker-and-rebuild-optimized-images.sh"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP":docker-tdj-images/thumbor-example/mapping.json mapping.json
 cat mapping.json
 rm -rf tdj-image-mapping
 ssh-agent bash -c 'ssh-add "$TDJ_IMAGE_MAP_DEPLOY_PRIVATE_KEY"; git clone git@github.com:alberto56/tdj-image-mapping.git'

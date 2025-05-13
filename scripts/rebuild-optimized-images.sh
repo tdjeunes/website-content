0751e9538b8f0d2271a41ef77d4a1e341f21e139
snamy groups <apple@apples-MacBook-Pro.local>
2025-05-13 10:37:46 +0530

    reverted

--- scripts/rebuild-optimized-images.sh
+++ scripts/rebuild-optimized-images.sh
@@ -7,4 +7,4 @@ set -e
 git clone https://github.com/dcycle/thumbor-example.git
 mv docs/media thumbor-example/my-media
 cd thumbor-example
-./scripts/generate-image-map.sh ./app/my-media contenu.terredesjeunes.org/media 800x,x200,x160,x200,75x75,600x680,x680 ./app/mapping.json
+./scripts/generate-image-map.sh ./app/my-media contenu.terredesjeunes.org/media 800x,x200,x160 ./app/mapping.json
